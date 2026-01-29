46b8448619c8775d7a03df1be730191a44fe2bc3
flywithbug <369495368@qq.com>
2026-01-29 16:29:22 +0800

    feat: box

--- src/box_tools/_share/openai_translate/__init__.py
+++ src/box_tools/_share/openai_translate/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from .client import OpenAIConfigError, resolve_api_key
 
 from .translate import translate_flat_dict, TranslationError
-from .json_translate import translate_from_to, JsonTranslateError
+from .translate_file import translate_from_to
 from .models import OpenAIModel
 
 from .translate_list import translate_list
@@ -13,7 +13,6 @@ __all__ = [
     'translate_flat_dict',
     'translate_from_to',
     'TranslationError',
-    'JsonTranslateError',
     'OpenAIModel',
     'OpenAIConfigError',
     'resolve_api_key',
